d87221dbc51707375453e93a287fbe9363ac13c7
albin <databasemindtree@gmail.com>
2026-08-06 11:14:33 +0530

    sitemap-update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,6 +8,18 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const staticRoutes: MetadataRoute.Sitemap = [
     { url: BASE_URL, lastModified: STATIC_LAST_MODIFIED },
     { url: `${BASE_URL}/blog`, lastModified: STATIC_LAST_MODIFIED },
+    {
+      url: `${BASE_URL}/blog/the-success-stories-of-nurses-who-have-chosen-mindtree-nursing-solution-for-their-careers`,
+      lastModified: STATIC_LAST_MODIFIED,
+    },
+    {
+      url: `${BASE_URL}/blog/why-is-mindtree-nursing-solution-the-best-choice-for-your-career`,
+      lastModified: STATIC_LAST_MODIFIED,
+    },
+    {
+      url: `${BASE_URL}/blog/what-is-mindtree-nursing-solution-and-what-it-offers`,
+      lastModified: STATIC_LAST_MODIFIED,
+    },
     { url: `${BASE_URL}/courses`, lastModified: STATIC_LAST_MODIFIED },
     {
       url: `${BASE_URL}/courses/best-oet-coaching-centre-in-kerala`,
